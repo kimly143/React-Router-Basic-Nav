dd6f6274961031f799b40f4db99f3ad6728abaeb
Kim Ly Buck <kim.3993@gmail.com>
2020-03-16 18:10:35 -0500

    finished all links

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const About = () => <h1>About</h1>;
+const About = () => (
+	<div>
+		<h1>About</h1>
+	</div>
+);
 
 export default About;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Home, About, Contact, Navigation } from './components';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Router>
+        <Navigation />
+		<Route exact path="/" component={Home} />
+		<Route path="/about" component={About} />
+		<Route path="/contact" component={Contact} />
+	</Router>,
+	document.getElementById('root')
+);
